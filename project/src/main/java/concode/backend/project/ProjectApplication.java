e96f5a5b62d295cf555a712d6d9587f9b3df0118
Henri Schlupp <schlupphenri@gmail.com>
2026-09-06 21:49:11 +0300

    Added basic backend test endpoint

--- project/src/main/java/concode/backend/project/ProjectApplication.java
+++ project/src/main/java/concode/backend/project/ProjectApplication.java
@@ -2,12 +2,12 @@ package concode.backend.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 public class ProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
 }
